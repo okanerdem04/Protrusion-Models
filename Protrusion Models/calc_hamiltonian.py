c811fd6acaa5c28ff9a211e793106b2394f1c9d5
okanterdem <okanterdem@gmail.com>
2025-10-13 16:52:15 +0100

    optimisations and change in adhesion energy calculation

--- Protrusion Models/calc_hamiltonian.py
+++ Protrusion Models/calc_hamiltonian.py
@@ -5,49 +5,44 @@ import random
 import numba
 
 @numba.njit
-def growth(lattice,cell_id,size,num_cells,ao,temp2): # calculates energy due to departure from targeted area
-    # inputs: 2d np array lattice, 1d np array cell_id, int size, int num_cells, int ao
-    # returns: int ham
+def find_areas(lattice,cell_id,width,height,num_cells): # find the areas of all cells
+    # inputs: 2d np array lattice, 1d np array cell_id, int width, int height, int num_cells, int a0, int alpha
+    # returns: 1d np array areas
 
-    # for each cell id, store the number of total lattice spaces it occupies
-    asig = np.zeros((num_cells+1), dtype=np.int64)
-    
-    ham = 0
+    # for each cell id, store the number of total lattice spaces it occupies (i.e. its area)
+    areas = np.zeros((num_cells+1), dtype=np.int64)
 
-    for i in range(size): # go through each cell
-        for j in range(size):
-            val = lattice[i][j]
-            asig[val] = asig[val] + 1 # increase the energy Asig for each cell id if the lattice space contains said id
-
-    for i in range(1,num_cells):
-        ham += (asig[i]-ao)**2 # increase the hamiltonian by the square of the difference of the size of the cell and its preferred size
+    for i in range(width): # go through each cell
+        for j in range(height):
+            areas[lattice[i][j]] += 1
         
-    # print(asig)
-    return ham*temp2 # return the value of the hamiltonian
+    return areas # return the value of the hamiltonian
 
 @numba.njit
-def tension(lattice,cell_id,size,num_cells,temp): # calculates energy due to surface tension of cell membranes
-    # inputs: 2d np array lattice, 1d np array cell_id, int size, int num_cells, int temp
-    # returns: int ham
+def neighbors(x, y, width, height): # takes an x and y position, returns all x and y positions directly adjacent to it
+    return [((x+1)%width, y), ((x-1)%width, y), (x, (y+1)%height), (x, (y-1)%height)]
 
+@numba.njit
+def adhesion_energy(lattice,width,height,x,y): # takes the lattice, goes through each cell, sums up how many different adjacents each cell has
+    energy = 0
+
+    for nx, ny in neighbors(x,y,width,height):
+        energy += (lattice[nx, ny] != lattice[x,y]) # adds to the energy depending on how many adjacent tiles belong to same cell
+
+    return energy
+
+def calc_hamiltonian(lattice,cell_id,width,height,num_cells,a0,alpha,lambd,x,y):
     ham = 0
 
-    for n in range(1,num_cells): # go through each individual cell
-        # then go through each cell space, adding 1 to ham for each adjacent space not belonging to cell
-        for i in range(size):
-            for j in range(size): # in theory we only need to do two comparisons for this to fully work?
-                if i < size-1: # in order to not access a thing outside of range, we do this
-                   if lattice[i][j] != lattice[i+1][j]: ham += 1
-                if j < size-1:
-                   if lattice[i][j] != lattice[i][j+1]: ham += 1
+    # area constraint energy
+    areas = find_areas(lattice,cell_id,width,height,num_cells)
+    for i in range(1,num_cells+1):
+        ham += lambd * ((areas[i]-a0) ** 2) # hamiltonian increases for each cell depending on how far its area is from the target
 
-    ham = ham * temp # multiply by our temperature, i.e. surface tension coefficient
-    return ham
+    # adhesion energy - just calculated for the swapped cells, since going through the entire grid takes a long time
+    adhesion = adhesion_energy(lattice,width,height,x,y)
+    ham += alpha*adhesion
 
-def calc_hamiltonian(lattice,cell_id,size,num_cells,ao,temp,temp2):
-    ham = 0
-    ham += tension(lattice,cell_id,size,num_cells,temp)
-    ham += growth(lattice,cell_id,size,num_cells,ao,temp2)
     return ham
 
 
--- Protrusion Models/main.py
+++ Protrusion Models/main.py
@@ -6,108 +6,70 @@ import matplotlib.pyplot as plt
 import matplotlib.animation
 import random
 import seaborn as sns
+import colorcet as cc
 # homemade functions
 from gen_lattice import gen_lattice
-from calc_hamiltonian import calc_hamiltonian
+from calc_hamiltonian import calc_hamiltonian, neighbors
 
-size = 100
-num_cells = 1
 
-lattice = np.zeros((size,size),dtype=np.int64) # lattice is stored as a 2d numpy array
-cell_id = np.array(range(1,num_cells)) # cell index array
+width = 200
+height = 200
+num_cells = 1000
+target_area = 40
+alpha = 2
+lambd = 1
 
-# gen_lattice(lattice,cell_id,size,num_cells) # fill up the lattice array
-width = 10
-height = 8
-a0 = 100
-lattice[10:10+width,10:10+height] = 1
+lattice = np.zeros((width,height),dtype=np.int64) # lattice is stored as a 2d numpy array
+cell_id = np.array(range(1,num_cells+2)) # cell index array
+random.shuffle(cell_id) # this is literally just done to get nicer colours
+
+# fill up the lattice with cells
+ind = 1
+for i in range(0,25):
+    x = i*8
+    for j in range(0,40):
+        y = j*5
+        lattice[x:x+8,y:y+5] = cell_id[ind]
+        ind += 1
+
+
 
 new_lattice = np.copy(lattice) # duplicate the old lattice, this allows a comparison later
 
-all_lattices = np.zeros((10000,size,size),dtype=np.int64)
-
-# loop 10000 times for now, should be adequate for now
 sweep = np.prod(new_lattice.shape)
-nsweeps = 20
+nsweeps = 10
 total_sweeps = nsweeps*sweep
-temp_lattice = new_lattice
+
 for i in range(total_sweeps):
-    # temp_lattice = np.copy(new_lattice)  
-    old_ham = calc_hamiltonian(new_lattice, cell_id, size, num_cells, a0, 50, 1) # hamiltonian with preferred size 100
+    x = random.randint(0,width-1)
+    y = random.randint(0,height-1)
 
-    rand_i = random.randint(0,size-1) # random i, j positions
-    rand_j = random.randint(0,size-1)
+    old_ham = calc_hamiltonian(new_lattice, cell_id, width, height, num_cells, target_area, alpha, lambd, x, y)
 
-
-    original = new_lattice[rand_i,rand_j] 
-    inv_i = rand_i
-    inv_j = rand_j
-
-    # pick a random neighbouring site to be "invading"
-    site_pick = random.randint(1,4)
-    if site_pick == 1:
-        # loop around the lattice if an edge is hit
-        if inv_i + 1 == size: inv_i = 0
-        else: inv_i += 1
-
-    elif site_pick == 2:
-        if inv_i - 1 == -1: inv_i = size-1
-        else: inv_i -= 1
-
-    elif site_pick == 3:
-        if inv_j + 1 == size: inv_j = 0
-        else: inv_j += 1
-
-    if site_pick == 4:
-        if inv_j - 1 == -1: inv_j = size-1
-        else: inv_j -= 1
+    original = new_lattice[x,y] 
+    Nx, Ny = neighbors(x,y,width,height)[np.random.randint(0, 4)] # selects a random adjacent position
 
     # check that invading site does not belong to same cell
-    if temp_lattice[rand_i][rand_j] != temp_lattice[inv_i][inv_j]:
+    if new_lattice[Nx,Ny] != new_lattice[x,y]:
         # if not, the picked site is replaced by the cell value of the invading site
-        temp_lattice[rand_i][rand_j] = temp_lattice[inv_i][inv_j]
+        new_lattice[x,y] = new_lattice[Nx,Ny]
         # calculate a new hamiltonian
-        new_ham = calc_hamiltonian(temp_lattice, cell_id, size, num_cells, a0, 5, 1) # hamiltonian with preferred size 10, surface tension coeff 0.2
+        new_ham = calc_hamiltonian(new_lattice, cell_id, width, height, num_cells, target_area, alpha, lambd, x, y)
 
-        energy_change = new_ham - old_ham # energy change
-        # print(f"energy change {energy_change}")
+        energy_change = new_ham - old_ham
 
-        # keep the change if energy is lowered or same
-        if energy_change <= 0:
-            new_lattice = temp_lattice
-            # print("lowered energy")
-        # else, calc probability to accept change
-        else:
-            prob = np.exp(-(energy_change)) # assume KbT = 1 (it's definitely not)
-            if random.random() <= prob:
-                new_lattice = temp_lattice
-                # print("random energy increase")
+        print(f"energy_change {energy_change}")
+        
 
-            else:
-                # print("no change")
-                new_lattice[rand_i,rand_j] = original
+        # if energy is increased, have a chance to replace the swapped cell
+        if energy_change > 0:
+            prob = np.exp(-(energy_change))
+            if random.random() > prob:
+                new_lattice[x,y] = original # this reverts changes to the lattice
+                print("change reverted")
+
     print(i,i//sweep, (new_lattice==1).sum())
-    # all_lattices[i] = np.copy(new_lattice)
 
-
-'''# animated heatmap shamelessly stolen from the internet
-def init_heatmap():
-    fig, ax = plt.subplots()
-    sns.heatmap(all_lattices[0], ax=ax, cbar=False, square=True)
-    return fig, ax
-
-def update_heatmap(frame, ax):
-    ax.clear()
-    sns.heatmap(all_lattices[frame], ax=ax, cbar=False, square=True)
-    print(f"anim frame {frame}")
-
-fig, ax = init_heatmap()
-anim = matplotlib.animation.FuncAnimation(fig, update_heatmap, frames=10000, fargs=(ax,), interval=30)
-
-anim.save('animated_heatmap.gif', writer='Pillow')'''
-
-sns.heatmap(new_lattice, cbar=False, square=True)
+fig, ax = plt.subplots()
+sns.heatmap(new_lattice,cmap=sns.color_palette("hls", num_cells))
 plt.show()
-
-# csv = pd.DataFrame(all_lattices)
-# csv.to_csv("testing_20_cells.csv")
