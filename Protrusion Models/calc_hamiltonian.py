ad2806a6cb44a2b4b900aec27e44ac3925ff67c4
okanterdem <okanterdem@gmail.com>
2025-10-14 18:44:18 +0100

    DO NOT USE; additions to code to attempt to simulate protrusion growth, did not work well

--- Protrusion Models/calc_hamiltonian.py
+++ Protrusion Models/calc_hamiltonian.py
@@ -10,7 +10,7 @@ def find_areas(lattice,cell_id,width,height,num_cells): # find the areas of all 
     # returns: 1d np array areas
 
     # for each cell id, store the number of total lattice spaces it occupies (i.e. its area)
-    areas = np.zeros((num_cells+1), dtype=np.int64)
+    areas = np.zeros((num_cells+1)*2, dtype=np.int64)
 
     for i in range(width): # go through each cell
         for j in range(height):
@@ -23,25 +23,58 @@ def neighbors(x, y, width, height): # takes an x and y position, returns all x a
     return [((x+1)%width, y), ((x-1)%width, y), (x, (y+1)%height), (x, (y-1)%height)]
 
 @numba.njit
+def diagonal_neighbors(x, y, width, height): # takes an x and y position, returns all x and y positions diagonal to it
+    return [((x+1)%width, y+1), ((x+1)%width, y-1), (x-1, (y+1)%height), (x-1, (y-1)%height)]
+
+@numba.njit
 def adhesion_energy(lattice,width,height,x,y): # takes the lattice, goes through each cell, sums up how many different adjacents each cell has
     energy = 0
 
     for nx, ny in neighbors(x,y,width,height):
-        energy += (lattice[nx, ny] != lattice[x,y]) # adds to the energy depending on how many adjacent tiles belong to same cell
+        # floor division by two to account for both cells and protrusions
+        energy += (lattice[nx,ny] != lattice[x,y]) # adds to the energy depending on how many adjacent tiles belong to different cell
 
     return energy
 
-def calc_hamiltonian(lattice,cell_id,width,height,num_cells,a0,alpha,lambd,x,y):
+@numba.njit
+def protrusion_incentive(lattice,width,height):
+    energy = 0
+
+    for i in range(width): # slightly disgusting loop through the whole grid
+        for j in range(height):
+            if lattice[i,j] % 2 == 1: # check to make sure cell at that lattice point is a protrusion
+                num_neighbours = 0
+                for nx, ny in neighbors(i,j,width,height):
+                    num_neighbours += (lattice[nx, ny] == lattice[i,j])
+
+                for nx, ny in diagonal_neighbors(i,j,width,height):
+                    num_neighbours += (lattice[nx, ny]//2 == lattice[i,j]//2)
+
+                if num_neighbours <= 2: energy -= num_neighbours # reduces energy if the cell has two neighbours exactly. this should convince protrusions to grown in a line, not a clump
+                elif num_neighbours >= 3: energy += num_neighbours # still discourage cells from being adjacent to more than two neighbours
+
+    return energy
+
+@numba.njit
+def calc_hamiltonian(lattice,cell_id,width,height,num_cells,a0,alpha,lambd,x,y,target_protrusions):
     ham = 0
 
     # area constraint energy
     areas = find_areas(lattice,cell_id,width,height,num_cells)
     for i in range(1,num_cells+1):
-        ham += lambd * ((areas[i]-a0) ** 2) # hamiltonian increases for each cell depending on how far its area is from the target
+        ham += lambd * ((areas[i*2]-a0) ** 2) # hamiltonian increases for each cell depending on how far its area is from the target
+
+        if areas[i*2 + 1] >= target_protrusions-1: # only apply to protrusions once they are close to their target area
+            ham += lambd * ((areas[i*2 + 1]-target_protrusions) ** 2) # apply the same principle for protrusions and their target area
 
     # adhesion energy - just calculated for the swapped cells, since going through the entire grid takes a long time
-    adhesion = adhesion_energy(lattice,width,height,x,y)
-    ham += alpha*adhesion
+    if lattice[x,y]%2 == 0: # only do adhesion energy for cell bodies, not protrusions
+        adhesion = adhesion_energy(lattice,width,height,x,y)
+        ham += alpha*adhesion
+
+    # protrusion incentive energy
+    protrusion = protrusion_incentive(lattice,width,height)
+    ham += protrusion
 
     return ham
 
--- Protrusion Models/main.py
+++ Protrusion Models/main.py
@@ -7,59 +7,54 @@ import matplotlib.animation
 import random
 import seaborn as sns
 import colorcet as cc
+import numba
 # homemade functions
 from gen_lattice import gen_lattice
 from calc_hamiltonian import calc_hamiltonian, neighbors
 
 
-width = 200
-height = 200
-num_cells = 1000
+width = 50
+height = 50
+num_cells = 1
 target_area = 40
-alpha = 2
+target_protrusions = 10
+alpha = 1.8
 lambd = 1
-
 lattice = np.zeros((width,height),dtype=np.int64) # lattice is stored as a 2d numpy array
-cell_id = np.array(range(1,num_cells+2)) # cell index array
-random.shuffle(cell_id) # this is literally just done to get nicer colours
+cell_id = np.multiply(np.array(range(0,num_cells+1)),2) # cell index array - store cells as even numbers
+protrusion_id = np.copy(cell_id) + 1 # protrusion index array - store protrusions as odd numbers
 
-# fill up the lattice with cells
-ind = 1
-for i in range(0,25):
-    x = i*8
-    for j in range(0,40):
-        y = j*5
-        lattice[x:x+8,y:y+5] = cell_id[ind]
-        ind += 1
-
+lattice[20:28,20:25] = 2
+lattice[19,24] = 3
 
 
 new_lattice = np.copy(lattice) # duplicate the old lattice, this allows a comparison later
 
 sweep = np.prod(new_lattice.shape)
-nsweeps = 10
+nsweeps = 50
 total_sweeps = nsweeps*sweep
+
 
 for i in range(total_sweeps):
     x = random.randint(0,width-1)
     y = random.randint(0,height-1)
 
-    old_ham = calc_hamiltonian(new_lattice, cell_id, width, height, num_cells, target_area, alpha, lambd, x, y)
+    old_ham = calc_hamiltonian(new_lattice, cell_id, width, height, num_cells, target_area, alpha, lambd, x, y, target_protrusions)
 
     original = new_lattice[x,y] 
     Nx, Ny = neighbors(x,y,width,height)[np.random.randint(0, 4)] # selects a random adjacent position
 
     # check that invading site does not belong to same cell
-    if new_lattice[Nx,Ny] != new_lattice[x,y]:
+    if (new_lattice[Nx,Ny]//2) != (new_lattice[x,y]//2):
         # if not, the picked site is replaced by the cell value of the invading site
         new_lattice[x,y] = new_lattice[Nx,Ny]
         # calculate a new hamiltonian
-        new_ham = calc_hamiltonian(new_lattice, cell_id, width, height, num_cells, target_area, alpha, lambd, x, y)
+        new_ham = calc_hamiltonian(new_lattice, cell_id, width, height, num_cells, target_area, alpha, lambd, x, y, target_protrusions)
 
         energy_change = new_ham - old_ham
 
         print(f"energy_change {energy_change}")
-        
+         
 
         # if energy is increased, have a chance to replace the swapped cell
         if energy_change > 0:
@@ -68,8 +63,8 @@ for i in range(total_sweeps):
                 new_lattice[x,y] = original # this reverts changes to the lattice
                 print("change reverted")
 
-    print(i,i//sweep, (new_lattice==1).sum())
+    print(i,i//sweep, (new_lattice==2).sum(), (new_lattice==3).sum())
 
 fig, ax = plt.subplots()
-sns.heatmap(new_lattice,cmap=sns.color_palette("hls", num_cells))
+sns.heatmap(new_lattice,cmap=sns.color_palette("hls", 4))
 plt.show()
