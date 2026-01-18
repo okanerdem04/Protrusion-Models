a27a55ff23295f603aacbf440cf95b583d772801
okanterdem <okanterdem@gmail.com>
2026-01-18 23:44:59 +0000

    final state of codebase prior to interim report submission; currently has initial parameters equal to that of the five-cell protrusion configuration (figure 3 of paper)

--- Protrusion Models/main.py
+++ Protrusion Models/main.py
@@ -14,25 +14,30 @@ from calc_hamiltonian import calc_hamiltonian, neighbors
 from protrusion_growth import center_of_mass, find_nearest_protrusion, protrusion_growth
 
 # starting variables:
-width = 100             # width of lattice
-height = 100            # height of lattice
-num_cells = 1           # number of unique cells
-target_area = 100       # target area of the body of cells
-target_prot = 10        # target area of the protrusions of cells
+width = 200             # width of lattice
+height = 200            # height of lattice
+num_cells = 5           # number of unique cells
+target_area = 500       # target area of the body of cells
+target_prot = 500        # target area of the protrusions of cells
 alpha = 1               # surface tension coefficient
-lambd = 5               # area constraint coefficient
+lambd = 2               # area constraint coefficient
 mu = 1                  # protrusion constraint coefficient
 
 # probability for the likelihood of a cell body being replaced by a protrusion (1 is guaranteed, 0 is never)
-protrusion_density = 0.2
+protrusion_density = 0.1
 
 lattice = np.zeros((width,height),dtype=np.int64) # lattice is stored as a 2d numpy array
 cell_id = np.multiply(np.array(range(1,num_cells+1)),3) # cell index array; this stores the value of the body parts of the cells
 # random.shuffle(cell_id) # this is literally just done to get nicer colours
 
 # set up initial conditions
-lattice[45:55,45:55] = cell_id[0]
-lattice[10,10] = 1
+lattice[90:110,90:110] = cell_id[0]
+lattice[40:60,40:60] = cell_id[1]
+lattice[140:160,40:60] = cell_id[2]
+lattice[40:60,140:160] = cell_id[3]
+lattice[140:160,140:160] = cell_id[4]
+
+
 
 '''lattice[45:55,145:155] = cell_id[1]
 lattice[145:155,45:55] = cell_id[2]
@@ -40,6 +45,7 @@ lattice[145:155,145:155] = cell_id[3]
 lattice[95:105,95:105] = cell_id[4]'''
 
 new_lattice = np.copy(lattice) # duplicate the old lattice, this allows a comparison later (TODO: get rid of this, it serves no purpose)
+snapshot_1 = np.copy(lattice)
 
 # calculate the value for the total number of sweeps, based on the size of the lattice
 sweep = np.prod(new_lattice.shape)
@@ -117,7 +123,13 @@ def run_mc(total_sweeps): # run the Monte Carlo simulation over the total number
                         new_lattice[new_protrusion_x,new_protrusion_y] = store
                         print("max protrusion length reached")
 
-        # print(i,i//sweep, (new_lattice==1).sum()) # this one line of code is really slow (probably because it calculates a remainder every single step)
+        #print(i,i//sweep, (new_lattice==1).sum()) # this one line of code is really slow (probably because it calculates a remainder every single step)
+        if i == 600000:
+            global snapshot_2
+            snapshot_2 = np.copy(new_lattice)
+        if i == 1200000:
+            global snapshot_3
+            snapshot_3 = np.copy(new_lattice)
 
 # run the Monte Carlo simulation without any protrusion cells for a certain amount of sweeps
 run_mc(budding_sweeps)
@@ -128,10 +140,40 @@ for i in range(width):
         if new_lattice[i][j] != 0 and random.random() < protrusion_density and new_lattice[i][j] % 3 == 0:
             new_lattice[i][j] = new_lattice[i][j] + 1
 
+# take a snapshot of lattice after this
+snapshot_1 = np.copy(new_lattice)
+
 # run the MC simulation again, this time with protrusion cells in the mix
 run_mc(protrusion_sweeps)
 
+palette = ["#ffffff", "#ffffff", "#ffffff", "#000000", "#999999", "#555555", "#000000", "#999999", "#555555", "#000000", "#999999", "#555555", "#000000", "#999999", "#555555", "#000000", "#999999", "#555555"]
+
 # plot the final lattice
-fig, ax = plt.subplots()
-sns.heatmap(new_lattice,cmap=sns.color_palette("hls", 6))
+fig, ax = plt.subplots(2,2)
+sns.heatmap(snapshot_1,cmap=sns.color_palette(palette,18),square=True,cbar=False,ax=ax[0,0],xticklabels=False, yticklabels=False, vmin=0,vmax=18)
+ax[0,0].annotate("a)",(10,20))
+ax[0,0].axhline(y = 0, color = '000000', linewidth = 3)
+ax[0,0].axhline(y = 200, color = '000000', linewidth = 3)
+ax[0,0].axvline(x = 0, color = '000000', linewidth = 3)
+ax[0,0].axvline(x = 200, color = '000000', linewidth = 3)
+sns.heatmap(snapshot_2,cmap=sns.color_palette(palette,18),square=True,cbar=False,ax=ax[0,1],xticklabels=False, yticklabels=False, vmin=0,vmax=18)
+ax[0,1].annotate("b)",(10,20))
+ax[0,1].axhline(y = 0, color = '000000', linewidth = 3)
+ax[0,1].axhline(y = 200, color = '000000', linewidth = 3)
+ax[0,1].axvline(x = 0, color = '000000', linewidth = 3)
+ax[0,1].axvline(x = 200, color = '000000', linewidth = 3)
+sns.heatmap(snapshot_3,cmap=sns.color_palette(palette,18),square=True,cbar=False,ax=ax[1,0],xticklabels=False, yticklabels=False, vmin=0,vmax=18)
+ax[1,0].annotate("c)",(10,20))
+ax[1,0].axhline(y = 0, color = '000000', linewidth = 3)
+ax[1,0].axhline(y = 200, color = '000000', linewidth = 3)
+ax[1,0].axvline(x = 0, color = '000000', linewidth = 3)
+ax[1,0].axvline(x = 200, color = '000000', linewidth = 3)
+sns.heatmap(new_lattice,cmap=sns.color_palette(palette,18),square=True,cbar=False,ax=ax[1,1],xticklabels=False, yticklabels=False, vmin=0,vmax=18)
+ax[1,1].annotate("d)",(10,20))
+ax[1,1].axhline(y = 0, color = '000000', linewidth = 3)
+ax[1,1].axhline(y = 200, color = '000000', linewidth = 3)
+ax[1,1].axvline(x = 0, color = '000000', linewidth = 3)
+ax[1,1].axvline(x = 200, color = '000000', linewidth = 3)
+
+plt.tight_layout()
 plt.show()
